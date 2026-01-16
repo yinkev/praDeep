cccb6acd7b93f2e488cd1b2fdcd463b90678a118
kyinhub <kevinpyin@gmail.com>
2026-01-16 04:30:32 -0800

    chore: add shadcn config

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -1,10 +1,6 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-/**
- * Utility function to merge Tailwind CSS classes
- * Combines clsx for conditional classes with tailwind-merge for deduplication
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
